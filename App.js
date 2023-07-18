e94a56a585c81d88eed0e10091233048c5885d34
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-07-18 15:54:30 -0600

    move states up to App()

--- App.js
+++ App.js
@@ -1,53 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Input from './src/components/Form';
+import React, {useState} from 'react';
+import { View, Text, TextInput, ScrollView } from 'react-native';
+import { Button } from 'react-native-web';
+// import Input from './src/components/Form';
 
 export default function App() {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+    petName: '',
+    petBirthDate: '',
+    breed: '',
+    favouriteToy: ''
+  });
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1'}}>
+    <ScrollView style={{backgroundColor: '#ecf0f1'}}>
       <Text style={{fontSize: 18, marginTop: 50}}>Registration</Text>
-      <ScrollView>
-        <Input 
-          title='Name of user'
-          placeholder='John Smith'
-          dataKey='userName'
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <InputWithLabel
+          label='Email'
+          placeholder='Type your email here'
+          value={input.email}
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              email : text
+            }))
+          }}
         />
-        <Input 
-          title='email'
-          placeholder="aa@gmail.com"
-          dataKey='email'
+        <InputWithLabel
+          label='Password'
+          placeholder="type your password here"
+          value={input.password}
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              password : text
+            }))
+          }}
+          secureTextEntry
         />
-        <Input 
-          title='password'
-          placeholder='empty'
-          dataKey='password'
+        <InputWithLabel
+          label='Confirm password'
+          placeholder='re-type your password here'
+          onSubmitEditing={(e) => confirmPassword(e.nativeEvent.text, input.password)}
+          secureTextEntry
         />
-        <Input 
-          title='type your password again'
-          placeholder='empty'
-          dataKey='passwordAgain'
+        <InputWithLabel
+          label='Pet Name'
+          value={input.petName}
+          placeholder="type your pet's name here"
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              petName : text
+            }))
+          }}
         />
-        <Input 
-          title='Your Pet Name'
-          placeholder='empty'
-          dataKey='petName'
+        <InputWithLabel
+          label='Pet Birthday'
+          value={input.petBirthDate}
+          placeholder="type your pet's date of birth here"
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              petBirthDate : text
+            }))
+          }}
         />
-        <Input 
-          title="Your pet's birthdate"
-          placeholder='empty'
-          dataKey='petBirthDate'
+        <InputWithLabel
+          label='Breed'
+          placeholder="type your pet's breed here"
+          value={input.breed}
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              breed : text
+            }))
+          }}
         />
-        <Input 
-          title="Your pet's breed"
-          placeholder='empty'
-          dataKey='breed'
+        <InputWithLabel
+          label='Toy'
+          placeholder="type your pet's favourite toy here"
+          value={input.favouriteToy}
+          onChangeText={text => {
+            setInput(prev => ({
+              ...prev,
+              favouriteToy : text
+            }))
+          }}
         />
-        <Input 
-          title="your pet's favourite toy"
-          placeholder='empty'
-          dataKey='FavouriteToy'
-        />
-      </ScrollView>
+      </View>
+    </ScrollView>
+  );
+};
+
+function confirmPassword (confirmPass, originalPassword) {
+  confirmPass === originalPassword 
+  ? true 
+  : alert("Passwords do not match, please try again.");
+};
+
+export function InputWithLabel (props) {
+  return (
+    <View style={{padding: 16}}>
+      <Text style={{padding: 8, fontSize: 18}}>{props.label}</Text>
+      <TextInput
+        style={{padding: 8, fontSize: 16}}
+        value={props.value}
+        secureTextEntry={props.secureTextEntry}
+        placeholder={props.placeholder}
+        onChangeText={props.onChangeText}
+        onSubmitEditing={props.onSubmitEditing}
+      />
     </View>
   );
-}
+};
