55155fa2f54ac599815b44af7a727c5cdde66d61
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-07-25 00:31:05 -0600

    use formik library for the same form

--- App.js
+++ App.js
@@ -1,96 +1,49 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { View, Text, TextInput, ScrollView } from 'react-native';
-import { Button } from 'react-native-web';
-// import Input from './src/components/Form';
+import {Formik, Field, Form, FieldArray, ErrorMessage} from 'formik';
 
 export default function App() {
-  const [input, setInput] = useState({
-    email: '',
-    password: '',
-    petName: '',
-    petBirthDate: '',
-    breed: '',
-    favouriteToy: ''
-  });
-
+  
   return (
     <ScrollView style={{backgroundColor: '#ecf0f1'}}>
       <Text style={{fontSize: 18, marginTop: 50}}>Registration</Text>
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <InputWithLabel
-          label='Email'
-          placeholder='Type your email here'
-          value={input.email}
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              email : text
-            }))
-          }}
-        />
-        <InputWithLabel
-          label='Password'
-          placeholder="type your password here"
-          value={input.password}
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              password : text
-            }))
-          }}
-          secureTextEntry
-        />
-        <InputWithLabel
-          label='Confirm password'
-          placeholder='re-type your password here'
-          onSubmitEditing={(e) => confirmPassword(e.nativeEvent.text, input.password)}
-          secureTextEntry
-        />
-        <InputWithLabel
-          label='Pet Name'
-          value={input.petName}
-          placeholder="type your pet's name here"
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              petName : text
-            }))
-          }}
-        />
-        <InputWithLabel
-          label='Pet Birthday'
-          value={input.petBirthDate}
-          placeholder="type your pet's date of birth here"
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              petBirthDate : text
-            }))
-          }}
-        />
-        <InputWithLabel
-          label='Breed'
-          placeholder="type your pet's breed here"
-          value={input.breed}
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              breed : text
-            }))
-          }}
-        />
-        <InputWithLabel
-          label='Toy'
-          placeholder="type your pet's favourite toy here"
-          value={input.favouriteToy}
-          onChangeText={text => {
-            setInput(prev => ({
-              ...prev,
-              favouriteToy : text
-            }))
-          }}
-        />
-      </View>
+      <Formik 
+        style={{flex: 1, justifyContent: 'center'}}
+        initialValues={{email: '', password: '', petName: '', petBirthDate: '', breed: '', favouriteToy: ''}}
+        vaildate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid Email Address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+      {({isSubmitting}) => (
+        <Form>
+          <Field type='email' name='email' />
+          <ErrorMessage name='email' component='div'/>
+          <Field type='password' name='password' />
+          <ErrorMessage name='password' component='div'/>
+          <Field type='petName' name='petName' />
+          <Field type='petBirthDate' name='petBirthDate' />
+          <Field type='breed' name='breed' />
+          <Field type='favouriteToy' name='favouriteToy' />
+          <button type='submit' disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+      </Formik>
     </ScrollView>
   );
 };
@@ -101,18 +54,3 @@ function confirmPassword (confirmPass, originalPassword) {
   : alert("Passwords do not match, please try again.");
 };
 
-export function InputWithLabel (props) {
-  return (
-    <View style={{padding: 16}}>
-      <Text style={{padding: 8, fontSize: 18}}>{props.label}</Text>
-      <TextInput
-        style={{padding: 8, fontSize: 16}}
-        value={props.value}
-        secureTextEntry={props.secureTextEntry}
-        placeholder={props.placeholder}
-        onChangeText={props.onChangeText}
-        onSubmitEditing={props.onSubmitEditing}
-      />
-    </View>
-  );
-};
