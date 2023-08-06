5726adebc65cb5292a90cc1f5741df0763ae617d
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-08-06 11:29:50 -0600

    Homescreen code in App.js, WoofCard component

--- App.js
+++ App.js
@@ -1,168 +1,194 @@
-import { AntDesign } from '@expo/vector-icons';
-import { ErrorMessage, Formik } from 'formik';
-import React from 'react';
-import { Button, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import * as Yup from 'yup';
+import * as React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  
-  return (
-    <ScrollView style={{flex: 1, backgroundColor: '#ecf0f1'}}>
-      <Text style={{marginTop: 50, fontSize: 20, textAlign: 'center'}}>Registration</Text>
-        <Formik
-          initialValues={{email: '', password: '', confirmPassword: '', petName: '', petBirthDate: '', breed: '', favouriteToy: ''}}
-          validationSchema={Yup.object({
-            email: Yup.string().email('Invalid email address').required('Required'),
-            password: Yup.string().required('Required'),
-            confirmPassword: Yup.string()
-              .required('Required')
-              .oneOf([Yup.ref('password')], 'Passwords do not match, please try again.'),
-            petName: Yup.string().required('Required'),
-            petBirthDate: Yup.string().required('Required'),
-            breed: Yup.string().required('Required'),
-            favouriteToy: Yup.string().required('Required')
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-        {({handleChange, handleBlur, handleSubmit, values, handleReset, isSubmitting, errors, touched}) => (
-          <View>
-              <InputWithLabel
-                label="Email"
-                name='email'
-                placeholder='Enter your email here'
-                value={values.email}
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                errors={errors}
-                touched={touched}
-              />
-              
-              <InputWithLabel
-                label='Password'
-                name='password'
-                placeholder="Type your password here"
-                value={values.password}
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                secureTextEntry
-                errors={errors}
-                touched={touched}
-              />
+// Basic reusable components
 
-              <InputWithLabel
-                label='Confirm Password'
-                name='confirmPassword'
-                value={values.confirmPassword}
-                placeholder="Re-type your password here"
-                onChangeText={handleChange('confirmPassword')}
-                secureTextEntry
-                errors={errors}
-                touched={touched}
-              />
+const Avatar = (props) => (
+  <Image
+    style={styles.avatar}
+    source={{ uri: props.url }}
+  />
+);
 
-              <InputWithLabel
-                label="Name"
-                name='petName'
-                placeholder="Type your dog's name here"
-                value={values.petName}
-                onChangeText={handleChange('petName')}
-                errors={errors}
-                touched={touched}
-              />
-              
-              <InputWithLabel
-                label='Birthday'
-                name='petBirthDate'
-                placeholder="Type your dog's date of birth here"
-                value={values.petBirthDate}
-                onChangeText={handleChange('petBirthDate')}
-                errors={errors}
-                touched={touched}
-              />
+const Heading = (props) => (
+  <Text style={styles.heading}>
+    {props.children}
+  </Text>
+);
 
-              <InputWithLabel
-                label='Breed'
-                name='breed'
-                placeholder="Type your dog's breed here"
-                value={values.breed}
-                onChangeText={handleChange('breed')}
-                errors={errors}
-                touched={touched}
-              />
+const Title = (props) => (
+  <Text style={styles.title}>
+    {props.name}
+  </Text>
+);
 
-              <InputWithLabel
-                label='Favourite toy'
-                name='favouriteToy'
-                placeholder="Type your dog's favorite toy here"
-                value={values.favouriteToy}
-                onChangeText={handleChange('favouriteToy')}
-                errors={errors}
-                touched={touched}
-              />
+const styles = StyleSheet.create({
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 60/2
+  },
+  heading: {
+    fontSize: 20,
+    margin: 8
+  },
+  title: {
+    margin: 7
+  },
+});
 
-              <Button
-                title='Submit'
-                onPress={handleSubmit}
-                disabled={isSubmitting}
-              />
-              <ResetButton 
-                handleReset={handleReset}
-                disabled={isSubmitting} 
-              />
-          </View>
-        )}
-      </Formik>
-    </ScrollView>
-  );
-};
+// App-specific components
 
-const InputWithLabel = (props) => {
+const WoofCard = (props) => (
+  <View style={woofCardStyles.card}>
+    <Avatar url={props.url}/>
+    <Title name={props.name}/>
+  </View>
+);
 
-  const moreStyle = props.errors[props.name] && props.touched[props.name] 
-    ? {borderWidth: 2, borderColor: 'red'} 
-    : {};
+const woofCardStyles = StyleSheet.create({
+  card: {
+    width: 100,
+    height: 113,
+    backgroundColor: '#FFFFE0',
+    borderRadius: 44/2,
+    margin: 5,
+    borderColor: 'black',
+    borderWidth: 2,
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  title: {},
+});
 
-  return (
-    <View style={{ padding: 16 }}>
-      <Text style={{padding: 8, fontSize: 18, marginLeft: 10}}>
-        {props.label}
-      </Text>
-      <TextInput
-        style={{padding: 8, fontSize: 16, marginLeft: 11, ...moreStyle}}
-        name={props.name}
-        onChangeText={props.onChangeText}
-        value={props.value}
-        placeholder={props.placeholder}
-        onBlur={props.onBlur}
-        secureTextEntry={props.secureTextEntry}
-      />
-      <ErrorMessage style={{padding: 8, marginLeft: 10, color: 'red'}} name={props.name} component={Text}/>
+const WoofPost = (props) => (
+  <View>
+    <Image source={{ uri: 'todo' }} />
+    <View>
+      <Text>todo</Text>
+      <Text>todo</Text>
     </View>
-  );
-};
+  </View>
+);
 
-const ResetButton = (props) => {
-  return (
-    <TouchableOpacity
-      accessible={true}
-      accessibilityLabel="Press to reset the form!"
-      onPress={props.handleReset}
-    > 
-      <Text style={{padding: 8, fontSize: 15, marginLeft: 10}}>
-        Click here to reset the Form!
-        <View style={{paddingLeft: 6}}>
-          <AntDesign
-            size={25}
-            name="closecircleo"
-            color="black"
-          />
-        </View>
-      </Text>
-    </TouchableOpacity>
-  )
-}
+const woofPostStyles = StyleSheet.create({
+  layout: {},
+  image: {},
+  content: {},
+  title: {},
+  description: {},
+});
+
+// The screen rendering everything
+const HomeScreen = () => (
+  <ScrollView style={{marginTop: 50}}>
+    <Heading>Trending Woofs</Heading>
+    <ScrollView horizontal={true}>
+      <WoofCard url="https://picsum.photos/64/64" name='REX'/>
+      <WoofCard url="https://picsum.photos/64/64" name='DANGO'/>
+      <WoofCard url="https://picsum.photos/64/64" name='DANGO 2'/>
+      <WoofCard url="https://picsum.photos/64/64" name='DANGO 3'/>
+      <WoofCard url="https://picsum.photos/64/64" name='DANGO 4'/>
+    </ScrollView>
+  </ScrollView>
+);
+
+const App = () => (
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#FAF9FA' }}>
+    <HomeScreen />
+  </SafeAreaView>
+);
+
+export default App;
+
+// "Fake" API data to use in your app
+const data = {
+  woofs: [
+    {
+      id: 'woof-1', 
+      name: 'Rex', 
+      avatar: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=648&q=80',
+      caretaker: 'Victor Grabarczyk',
+      source: 'https://unsplash.com/photos/x5oPmHmY3kQ',
+    },
+    {
+      id: 'woof-2', 
+      name: 'Ball', 
+      avatar: 'https://images.unsplash.com/photo-1585584114963-503344a119b0?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Tatiana Rodriguez',
+      source: 'https://unsplash.com/photos/J40C1k6Fut0',
+    },
+    {
+      id: 'woof-3', 
+      name: 'Happy', 
+      avatar: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Marliese Streefland',
+      source: 'https://unsplash.com/photos/2l0CWTpcChI',
+    },
+    {
+      id: 'woof-4',
+      name: 'Fluffy',
+      avatar: 'https://images.unsplash.com/photo-1554956615-1ba6dc39921b?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Nick Fewings',
+      source: 'https://unsplash.com/photos/rMKXLAIa2OY',
+    },
+    {
+      id: 'woof-5',
+      name: 'Spirit',
+      avatar: 'https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?auto=format&fit=crop&h=64&q=80',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/uNNCs5kL70Q',
+    },
+  ],
+  posts: [
+    {
+      id: 'post-1',
+      image: 'https://images.unsplash.com/photo-1544568100-847a948585b9?auto=format&fit=crop&w=967&q=80',
+      title: 'Happy Woofs',
+      description: 'How to keep your woof health and happy. We\'ve asked some of the best experts out there.',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/UtrE5DcgEyg',
+    },
+    {
+      id: 'post-2',
+      image: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?auto=format&fit=crop&w=850&q=80',
+      title: 'Woofs & friends',
+      description: 'Best friends are important for humans, but also for dogs.',
+      caretaker: 'Krista Mangulsone',
+      source: 'https://unsplash.com/photos/9gz3wfHr65U',
+    },
+    {
+      id: 'post-3',
+      image: 'https://images.unsplash.com/photo-1558947530-cbcf6e9aeeae?auto=format&fit=crop&w=634&q=80',
+      title: 'Good Woofs',
+      description: 'A good woof is a woof that brings joy. Here are a few tips to let your woof behave.',
+      caretaker: 'Olia Nayda',
+      source: 'https://unsplash.com/photos/f6v_Q0WXEK8',
+    },
+    {
+      id: 'post-4',
+      image: 'https://images.unsplash.com/photo-1444212477490-ca407925329e?auto=format&fit=crop&w=1100&q=80',
+      title: 'Wild Woofs',
+      description: 'In some parts of the world, wild woofs are very common. Learn how to interact in a nice way.',
+      caretaker: 'Anoir Chafik',
+      source: 'https://unsplash.com/photos/2_3c4dIFYFU',
+    },
+    {
+      id: 'post-5',
+      image: 'https://images.unsplash.com/photo-1567014543648-e4391c989aab?auto=format&fit=crop&w=1050&q=80',
+      title: 'Sleepy Woofs',
+      description: 'Sleeping is just as important for woofs as it is for humans. What are the main things your woof dreams about.',
+      caretaker: 'Max Singh',
+      source: 'https://unsplash.com/photos/2637Pic9xMw',
+    },
+    {
+      id: 'post-6',
+      image: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?auto=format&fit=crop&w=967&q=80',
+      title: 'Exploring Woofs',
+      description: 'Just sitting in one place is boring for most woofs. How do woofs explore the world?',
+      caretaker: 'Jamie Street',
+      source: 'https://unsplash.com/photos/wcO2PWLuQ3U',
+    },
+  ],
+};
